9264b2529c144f2a2bc6ac332239be69b7d96220
Judah0715 <judah.agnew121@gmail.com>
2022-12-07 16:04:17 -0500

    Added Functionality Code

--- most-wanted/app.js
+++ most-wanted/app.js
@@ -273,3 +273,17 @@ function searchByTraits(people){
             traitChoose = prompt('keep choosing? yes or no: ')
             
             break;
+
+            case "Eye Color":
+                let traitEyeColors = promptFor('Enter Eye Color or skip: ', chars)
+                people = people.filter(function (el){
+                    if(el.eyeColor == traitEyeColors)
+                        return true;
+                            
+                })
+                alert(people.map(function (person){
+                    return `${person.firstName} ${person.lastName}`;
+                    })
+                    .join("\n"))
+                traitChoose = prompt('keep choosing? yes or no: ')
+                break;   
